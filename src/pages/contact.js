d18585aaf3b730d2b8b98a138a4959590dcb03a4
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-11-16 15:29:13 +0100

    add pages

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Contact me by mail @ wilfried.lloza@gmail.com</p>
+    </div>
+  )
+}
+
+export default ContactPage
