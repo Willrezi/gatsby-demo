d7fdf677bb5022564f771dcf03b25b420fac6f00
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-12-15 22:03:36 +0100

    rendering images from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import propTypes from "prop-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 
@@ -20,15 +21,31 @@ export const query = graphql`
     contentfulBlogPost(slug: { eq: $slug }) {
       title
       publishedDate(formatString: "DD MMMM YYYY")
+      body {
+        json
+      }
     }
   }
 `
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["fr"]
+        const url = node.data.target.fields.file["fr"].url
+        return <img alt={alt} src={url} />
+      },
+    },
+  }
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p>{props.data.contentfulBlogPost.publishedDate}</p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   )
 }
