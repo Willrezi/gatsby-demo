971b7940b142f4e93c2cba013c03379b5e9b952b
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-12-15 15:43:47 +0100

    dynamic pages

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -4,27 +4,31 @@ import propTypes from "prop-types"
 
 import Layout from "../components/layout"
 
+// export const query = graphql`
+//   query markdownRemark($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 export const query = graphql`
-  query markdownRemark($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
+  query($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "DD MMMM YYYY")
     }
   }
 `
+
 const Blog = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: props.data.markdownRemark.html,
-        }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
     </Layout>
   )
 }
