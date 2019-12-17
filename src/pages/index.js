a5471740d2a82ef09097db5f5c79b32e6d7078d4
Wilfried LLOZA <wilfried.lloza@gmail.com>
2019-12-17 22:21:15 +0100

    clean up home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I'm Wilfried, a full-stack developper living in Paris</h2>
-      <p>
-        Need a developper ? <Link to="/contact">Contact me.</Link>{" "}
-      </p>
     </Layout>
   )
 }
