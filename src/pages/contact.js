f952d3fe175918b0c2dc0f89a9b63907812a780e
Wilfried LLOZA <wilfried.lloza@gmail.com>
2020-01-09 22:04:25 +0100

    test forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const ContactPage = () => {
       <Head title="Contact" />
       <h1>Contact</h1>
       <p>Contact me by mail @ wilfried.lloza@gmail.com</p>
-      <form name="contact" method="POST" netlify>
+      <form name="contact" method="POST" data-netlify="true">
         <input type="email" name="email" placeholder="Votre email" />
         <button type="submit">OK</button>
       </form>
